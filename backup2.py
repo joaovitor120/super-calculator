bf97a19dc482df38df456773c21f1da6f160edd3
joaovitor120 <jvbindacaetano.dev@gmail.com>
2026-02-11 23:15:26 -0300

    REORGANIZING FUNCTIONS
    now i'm separating functions responsabilities, like a function only for output, and a function only to receive input.

--- backup2.py
+++ backup2.py
@@ -11,9 +11,9 @@ operations_available = {
 def WelcomeUser():
     bdayYear = False
     year = int(input("Which year we are? "))
-    bday = input("Do you already make birthiday this year?(Y/N)").upper()
+    bday = input("Do you already make birthiday this year?")
 
-    if bday == "Y":
+    if bday == "S":
         bdayYear = True
     name = input("What is your name? ")
     age = int(input("How old are you? "))
@@ -29,7 +29,7 @@ def WelcomeUser():
     return user
 
 #function output
-def show_user_data(user):
+def userInfo(user):
     for i in user:
         user_datas = f"{i}:{user[i]}"
         print(user_datas)
@@ -43,35 +43,31 @@ def validate_number(msg):
             return int(input(msg))
         except ValueError:
             print("Invalid number. Try again.")
-def get_valid_operation(operation):
-        while operation not in operations_available:
-            print("Invalid operation, select only one of them:(+,-,/,*)")
-            operation = input("Which operation do you wanna make((+)/(-)/(*)/(/))? ").strip()
-        return operation
-            
-            
-def get_operation():
-    return input("Which operation do you wanna make((+)/(-)/(*)/(/))? ").strip()
 
-def get_numbers():
+def get_user_input():
+    calcType = input("Which operation do you wanna make((+)/(-)/(*)/(/))? ")
+    while calcType not in operations_available:
+        print("You type an invalid operation simbol.")
+        calcType = input("Type which operation do you wanna make among them((+)/(-)/(*)/(/))? ")
     num1 = validate_number(("Type the first number: "))
     num2 = validate_number(("Type the second number: "))
-    return num1, num2
-def calculate(op,n1,n2):
-    result = operation(op, n1, n2)
+    return calcType, num1, num2
+
+def calc_process(user):
+    calcType, num1, num2 = get_user_input()
+    result = operation(calcType, num1, num2)
     return result
 
-def exit_program(user,op,n1,n2):
+def exit_program(user):
     return "EXIT"
-
 menu = {
-    "1": calculate,
-    "2": show_user_data,
+    "1": calc_process,
+    "2": userInfo,
     "3": exit_program
 }
 
 def menufunc():
-    print("\n MENU: \n 1:Calculator \n 2:My Informations \n 3:Exit")
+    #print("\n MENU: \n 1:Calculator \n 2:My Informations \n 3:Exit")
     optionmenu = input("Choose one of them options(1/2/3): ")
     while optionmenu not in menu:
         print("Invalid option. Please choose 1, 2, or 3.")
@@ -82,21 +78,18 @@ def menufunc():
 def main():
     user = WelcomeUser()
     print(f"Welcome, Mr {user['Name']}, born in {user['Year Born']}, you receive an access to the JVBCalculator")
-    
     while True:
-        optionmenu = menufunc()
-        if optionmenu == "1":
-            try:
-                calctype = get_operation()
-                operation = get_valid_operation(calctype)
-                num1, num2 = get_numbers()
-                result = menu[optionmenu](operation,num1,num2)
-                print(f"Result {result}")
-            except ZeroDivisionError:
-                print("Division by zero is not allowed")
-        elif optionmenu == "2":
+        
+        try:
+            optionmenu = menufunc()
             result = menu[optionmenu](user)
-        elif optionmenu == "3":
+            if result != "EXIT" and optionmenu != "2":
+                print(f"Result: {result}")
+        except ZeroDivisionError:
+            print("Division by zero is not allowed")
+        #again = input("Do you wanna continue (Y/N)? ").upper()
+
+        if result == "EXIT":
             print("Goodbye! See you later!")
             break
         time.sleep(1)
---
+++ test.py
@@ -0,0 +1,11 @@
+def printA(a):
+    print(a)
+def printB(b):
+    print(b)
+
+menu = {
+    "1": printA,
+    "2": printB
+}
+
+menu["1"]("asdsa")
